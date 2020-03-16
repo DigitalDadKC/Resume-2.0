31e021f46d8a635c19df7b6f9564caab714b398b
DigitalDadKC <raleighgroesbeck@hotmail.com>
2020-03-16 07:20:01 -0500

    Delete main.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function Circle(el){
-  $(el).circleProgress({fill: {color: '#4472c4'}})
-    .on('circle-animation-progress', function(event, progress, stepValue){
-					$(this).find('strong').text(String(stepValue.toFixed(2)).substr(2)+'%');
-			});
-};
-Circle('.round');
